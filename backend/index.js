b85e8aa5150e792628eafdb9ade09d81c8800335
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-06-10 20:52:07 +0400

    Чуть более сложные настройки (body, query string, params)

--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,79 @@ app.get("/", (req, res) => {
   res.send("Hello from server!!!!");
 });
 
+// ----------------- 1 --------------
+
+// app.post("/users", (req, res) => {
+//   res.json([
+//     { id: 1, name: "Jane Doe" },
+//     { id: 2, name: "Ya vtoroi" },
+//   ]);
+// });
+
+// app.get("/users", (req, res) => {
+//   res.json([
+//     { id: 1, name: "Jane Doe" },
+//     { id: 2, name: "Ya vtoroi" },
+//   ]);
+// });
+
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params.id;
+
+//   res.json({ id: id, name: "Fake name" });
+// });
+
+// app.delete("/users/:id", (req, res) => {
+//   res.json(1);
+// });
+
+// ----------------- 2 --------------
+
+// request params
+
+// app.get("/users/:userId/profiles/:profileId", (req, res) => {
+//   console.log(req.params);
+
+//   const { userId, profileId } = req.params;
+
+//   res.json({ id: userId, name: "Fake name", profileId });
+
+//   console.log(userId, profileId);
+// });
+
+// request query
+
+// app.get("/users/:userId/profiles/:profileId", (req, res) => {
+//   const queryStringValues = req.query;
+
+//   const { userId, profileId } = req.params;
+
+//   res.json({
+//     id: userId,
+//     name: "Fake name",
+//     profileId,
+//     queryStringValues,
+//   });
+
+//   console.log(userId, profileId, queryStringValues);
+// });
+
+// request body
+
+app.use(express.json());
+
+app.post("/users/:userId/profiles/:profileId", (req, res) => {
+  const queryStringValues = req.query;
+  const params = req.params;
+  const body = req.body;
+
+  res.json({
+    params,
+    queryStringValues,
+    body,
+  });
+
+  console.log(params, queryStringValues, body);
+});
+
 export default app;
