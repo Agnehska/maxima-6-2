f1c77ecdfa3034d852c707a6a9dadf862b6ac71e
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-06-13 21:43:48 +0400

    Добавили бэкенд в коннекте с базой данных

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+import todoRouter from "./todo/todo-router.js";
+
+app.use(express.json());
+
+app.use("/todos", todoRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello from express server!");
+});
+
+export default app;
