2e83e78973b081be9a80c23f74b6a25a6dbc702b
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-06-20 21:14:31 +0400

    Подключили клерк

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,52 +4,71 @@ import { Context } from "./index";
 import { observer } from "mobx-react-lite";
 import { IUser } from "./models/IUser";
 import UserService from "./services/UserService";
+import { SignIn, SignInButton, SignedOut, UserButton } from '@clerk/clerk-react';
 
 const App: FC = () => {
-    const { store } = useContext(Context);
-    const [users, setUsers] = useState<IUser[]>([]);
+    // const { store } = useContext(Context);
+    // const [users, setUsers] = useState<IUser[]>([]);
 
-    useEffect(() => {
-        if (localStorage.getItem('token')) {
-            store.checkAuth()
-        }
-    }, [])
+    // useEffect(() => {
+    //     if (localStorage.getItem('token')) {
+    //         store.checkAuth()
+    //     }
+    // }, [])
 
-    async function getUsers() {
-        try {
-            const response = await UserService.fetchUsers();
-            setUsers(response.data);
-        } catch (e) {
-            console.log(e);
+    // async function getUsers() {
+    //     try {
+    //         const response = await UserService.fetchUsers();
+    //         setUsers(response.data);
+    //     } catch (e) {
+    //         console.log(e);
 
-        }
-    }
+    //     }
+    // }
 
-    if (store.isLoading) {
-        return <div>Загрузка...</div>
-    }
+    // if (store.isLoading) {
+    //     return <div>Загрузка...</div>
+    // }
 
-    if (!store.isAuth) {
-        return (
-            <div>
-                <LoginForm />
-                <button onClick={getUsers}>Получить пользователей</button>
-            </div>
-        );
-    }
+    // if (!store.isAuth) {
+    //     return (
+    //         <div>
+    //             <LoginForm />
+    //             <button onClick={getUsers}>Получить пользователей</button>
+    //         </div>
+    //     );
+    // }
 
     return (
-        <div>
-            <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
-            <h1>{store.user.isActivated ? 'Аккаунт подтвержден по почте' : 'ПОДТВЕРДИТЕ АККАУНТ!!!!'}</h1>
-            <button onClick={() => store.logout()}>Выйти</button>
+        <>
+            {/* <div>
+                <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
+                <h1>{store.user.isActivated ? 'Аккаунт подтвержден по почте' : 'ПОДТВЕРДИТЕ АККАУНТ!!!!'}</h1>
+                <button onClick={() => store.logout()}>Выйти</button>
+                <div>
+                    <button onClick={getUsers}>Получить пользователей</button>
+                </div>
+                {users.map(user =>
+                    <div key={user.email}>{user.email}</div>
+                )}
+            </div> */}
+
             <div>
-                <button onClick={getUsers}>Получить пользователей</button>
+                <SignIn>
+                    <p>Тут ты авторизован</p>
+
+                    <UserButton>Профиль</UserButton>
+                </SignIn>
             </div>
-            {users.map(user =>
-                <div key={user.email}>{user.email}</div>
-            )}
-        </div>
+
+            <div>
+                <SignedOut>
+                    А тут ты не авторизован
+
+                    <SignInButton>Войти</SignInButton>
+                </SignedOut>
+            </div>
+        </>
     );
 };
 
